6decbf29f40aa43d0d104bbb64c50f9238363e82
Shalem Raju <shalemrajubiraja7@gmail.com>
2026-08-22 17:14:23 +0530

    Spring boot project setup

---
+++ ship-track-backend/src/main/java/com/ship_track_backend/ShipTrackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ship_track_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipTrackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipTrackBackendApplication.class, args);
+	}
+
+}

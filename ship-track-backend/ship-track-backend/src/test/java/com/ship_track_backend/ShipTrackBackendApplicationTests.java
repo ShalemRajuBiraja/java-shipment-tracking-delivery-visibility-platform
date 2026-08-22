8ef2c70c52d4a7a5adef3b2f536dca152a890e10
Shalem Raju <shalemrajubiraja7@gmail.com>
2026-08-22 16:28:34 +0530

    Spring project setup

---
+++ ship-track-backend/ship-track-backend/src/test/java/com/ship_track_backend/ShipTrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ship_track_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipTrackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

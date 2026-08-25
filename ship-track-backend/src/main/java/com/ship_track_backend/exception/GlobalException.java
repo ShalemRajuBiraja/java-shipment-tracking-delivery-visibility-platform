2374593f3d16a2564ec25566302b321a31c0ff46
Shalem Raju <shalemrajubiraja7@gmail.com>
2026-08-25 18:28:07 +0530

    Created Auth files

---
+++ ship-track-backend/src/main/java/com/ship_track_backend/exception/GlobalException.java
@@ -0,0 +1,5 @@
+package com.ship_track_backend.exception;
+
+public class GlobalException {
+
+}
---
+++ ship-track-backend/src/main/java/com/ship_track_backend/repository/AuthRepository.java
@@ -0,0 +1,7 @@
+package com.ship_track_backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthRepository extends JpaRepository<T, ID> {
+
+}
